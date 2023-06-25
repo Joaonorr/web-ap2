697c3d342019750d755880963f81421479c3a5f1
Joaonorr <joaonoroes.jn@gmail.com>
2023-06-25 17:02:24 -0300

    feat: definindo modelo dos alunos

---
+++ Backend/src/models/AlunoModel.js
@@ -0,0 +1,32 @@
+/* -----------------------------
+ * Definição do modelo de Aluno
+ * 
+ * Atributos:
+ * - nome: string
+ * - curso: string
+ * - ira: float
+ * -----------------------------
+ */
+
+import { Sequelize } from 'sequelize';
+import DbContext from '../config/mongooseConnection.js';
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const Aluno = new Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    curso: {
+        type: String,
+        required: true,
+    },
+    ira: {
+        type: Number,
+        required: true,
+    },
+});
+
+export default mongoose.model('Aluno',Aluno);
