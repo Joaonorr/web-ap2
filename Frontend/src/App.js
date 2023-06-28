bf4472211d59e1ffc11310e9e30dece369e03a75
augustces <augustces@alu.ufc.br>
2023-06-28 10:03:43 -0300

    Adicionando telas de cadastrar e editar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ListarAlunos from './components/Alunos/ListarAlunos';
+
 import Create from './components/create';
 
 function App() {
   return (
     <div className="main">
       <div>
-        <ListarAlunos />
+        <ListarAlunos/>
       </div>
     </div>
   );
